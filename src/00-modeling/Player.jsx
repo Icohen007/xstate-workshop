62d631fe755986214bc9aeb252f831d69f923715
Itamar Cohen <itamarc@fb.com>
2022-09-14 10:26:37 +0300

    Modeling - add simple state machine + button onClick - with support of play, pause, skip, loading states.

--- src/00-modeling/Player.jsx
+++ src/00-modeling/Player.jsx
@@ -1,64 +1,97 @@
-import { useEffect } from 'react';
-import { useReducer } from 'react';
+import {useEffect} from 'react';
+import {useReducer} from 'react';
 
 const initialState = {
   value: 'loading', // or 'playing' or 'paused'
 };
 
-function playerMachine(state, event) {
-  // Use state machine
+const machineDefinition = {
+  states: {
+    paused: {
+      on: {
+        play: 'playing',
+        skip: 'loading'
+      }
+    },
+    loading: {
+      on: {
+        pause: 'paused',
+        LOADED: 'paused',
+        skip: 'loading'
+      }
+    },
+    playing: {
+      on: {
+        pause: 'paused',
+        skip: 'loading'
+      }
+    },
+  }
+}
 
-  return state;
+function playerMachine(state, event) {
+  const stateDefinition = machineDefinition.states[state.value];
+  const nextStateValue = stateDefinition?.on[event.type] ?? state.value;
+
+  return {...state, value: nextStateValue};
 }
 
 export function Player() {
   const [state, send] = useReducer(playerMachine, initialState);
 
+  console.log(state);
+
   useEffect(() => {
+
+    if (state.value !== 'loading') {
+      return;
+    }
     const i = setTimeout(() => {
-      send({ type: 'LOADED' });
+      send({type: 'LOADED'});
     }, 1000);
 
     return () => {
       clearTimeout(i);
     };
-  }, []);
+  }, [state.value]);
 
   return (
-    <div id="player">
-      <div className="song">
-        <div className="title">
-          <em>Song Title</em>
+      <div id="player">
+        <div className="song">
+          <div className="title">
+            <em>Song Title</em>
+          </div>
+          <div className="artist">
+            <em>Artist</em>
+          </div>
+          <input type="range" id="scrubber" min="0" max="0"/>
+          <output id="elapsed"></output>
         </div>
-        <div className="artist">
-          <em>Artist</em>
+        <div className="controls">
+          <button id="button-like"></button>
+          <button id="button-dislike"></button>
+          {state.value === 'paused' && (
+              <button
+                  id="button-play"
+                  onClick={() => {
+                    send({type: 'play'});
+                  }}
+              ></button>
+          )}
+          {state.value === 'playing' && (
+              <button
+                  id="button-pause"
+                  onClick={() => {
+                    send({type: 'pause'})
+                  }}
+              ></button>
+          )}
+          {state.value === 'loading' && <button id="button-loading"></button>}
+          <button id="button-skip" onClick={() => {
+            send({type: 'skip'})
+          }}></button>
+          <button id="button-volume"></button>
         </div>
-        <input type="range" id="scrubber" min="0" max="0" />
-        <output id="elapsed"></output>
       </div>
-      <div className="controls">
-        <button id="button-like"></button>
-        <button id="button-dislike"></button>
-        {state.value === 'paused' && (
-          <button
-            id="button-play"
-            onClick={() => {
-              // Send a 'PLAY' event
-            }}
-          ></button>
-        )}
-        {state.value === 'playing' && (
-          <button
-            id="button-pause"
-            onClick={() => {
-              // Send a 'PAUSE' event
-            }}
-          ></button>
-        )}
-        {state.value === 'loading' && <button id="button-loading"></button>}
-        <button id="button-skip"></button>
-        <button id="button-volume"></button>
-      </div>
-    </div>
   );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-// import { Player } from './00-modeling/Player';
+import { Player } from './00-modeling/Player';
 // import { Player } from './01-states-transitions/Player';
 // import { Player } from './02-actions/Player';
 // import { Player } from './03-context/Player';
@@ -9,36 +9,37 @@ import './App.css';
 // import { Player } from './07-final-states/Player';
 // import { Player } from './08-history-states/Player';
 // import { Player } from './09-actors/Player';
-
-function Player() {
-  return (
-    <div id="player">
-      <div className="song">
-        <div className="title">
-          <em>Song Title</em>
-        </div>
-        <div className="artist">
-          <em>Artist</em>
-        </div>
-        <input type="range" id="scrubber" min="0" max="0" />
-        <output id="elapsed"></output>
-      </div>
-      <div className="controls">
-        <button id="button-like"></button>
-        <button id="button-dislike"></button>
-        <button id="button-play"></button>
-        <button id="button-pause"></button>
-        <button id="button-loading"></button>
-        <button id="button-skip"></button>
-        <button id="button-volume"></button>
-      </div>
-    </div>
-  );
-}
+//
+// function Player() {
+//   return (
+//     <div id="player">
+//       <div className="song">
+//         <div className="title">
+//           <em>Song Title</em>
+//         </div>
+//         <div className="artist">
+//           <em>Artist</em>
+//         </div>
+//         <input type="range" id="scrubber" min="0" max="0" />
+//         <output id="elapsed"></output>
+//       </div>
+//       <div className="controls">
+//         <button id="button-like"></button>
+//         <button id="button-dislike"></button>
+//         <button id="button-play"></button>
+//         <button id="button-pause"></button>
+//         <button id="button-loading"></button>
+//         <button id="button-skip"></button>
+//         <button id="button-volume"></button>
+//       </div>
+//     </div>
+//   );
+// }
 
 function App() {
   return (
     <div className="App">
+      <h1 className="player-title">Itamar's Song Player</h1>
       <Player />
     </div>
   );
